a0db4f22888a6ee8053580a7d4758748e859becc
Jan Illetschko <jan.illetschko@3kraft.com>
2017-12-05 19:48:22 +0100

    simple dom manipulation with jquery

--- js/main.js
+++ js/main.js
@@ -2,12 +2,20 @@
     window.console.log("running main.js");
 
     var addH1 = function(text) {
-        document.body.innerHTML = "<h1>" + text + "</h1>";
+        $("body").append("<h1>" + text + "</h1>");
     };
 
-    window.setTimeout(function() {
     $(document).ready(function() {
         window.console.log("document loaded");
         addH1("Hallo");
+        var i = 1;
+        var timer = setInterval(function() {
+            if (i < 10) {
+                addH1("Hallo " + (i++));
+            } else {
+                window.console.log("done");
+                clearInterval(timer);
+            }
+        }, 1000);
     });
 })(jQuery);
