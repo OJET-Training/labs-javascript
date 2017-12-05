1755e67e81aafa64ad0cb6faf3ce6e6da2a2e7f5
Jan Illetschko <jan.illetschko@3kraft.com>
2017-12-05 19:11:12 +0100

    namespace conflict

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 window.console.log("running main.js");
+
+var addH1 = function(text) {
+    document.body.innerHTML = "<h1>" + text + "</h1>";
+};
+
+window.setTimeout(function() {
+    addH1("Hallo");
+}, 5000);
---
+++ js/second.js
@@ -0,0 +1,3 @@
+var addH1 = function(text) {
+    document.body.innerHTML = "<h1>" + text.toUpperCase() + "</h1>";
+};
